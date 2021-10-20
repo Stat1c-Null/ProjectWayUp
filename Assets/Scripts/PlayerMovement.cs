4f84b2cc4efa1a9ce1aa534eda4bf7509d33243d
Mikita Slabysh <slabysh2015@gmail.com>
2021-10-20 11:20:03 -0400

    Glitchy Walls

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,11 @@ public class PlayerMovement : MonoBehaviour
     public float movementSpeed;
     public Rigidbody2D rb;
     float mx;
+    public Transform feet;
+    public float jumpForce;
+    public LayerMask groundLayers;//Return layer on which player stands
+
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +21,37 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Horizontal input
         mx = Input.GetAxisRaw("Horizontal");
+        //Jumping
+        if(Input.GetButtonDown("Jump") && IsGrounded())//Dont let player jump unless his on the ground
+        {
+            Jump();
+        }
+
+        //ANIMATIONS
+        //Turn on Running animation if moving
+        //Absolute always returns you positive value
+        if(Mathf.Abs(mx) > 0.05f)
+        {
+            anim.SetBool("IsRunning", true);
+        } else
+        {
+            anim.SetBool("IsRunning", false);
+        }
+
+        //Make player turn different sides
+        if (mx > 0f)
+        {
+            transform.localScale = new Vector3(4f, 4f, 4f);
+        }
+        else if(mx < 0f)
+        {
+            transform.localScale = new Vector3(-4f, 4f, 4f);
+        }
+
+        //Change into jumping animation
+        anim.SetBool("IsGrounded", IsGrounded());//Make function return whether his grounded or not
     }
 
     private void FixedUpdate()
@@ -24,5 +59,25 @@ public class PlayerMovement : MonoBehaviour
         Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
 
         rb.velocity = movement;
+    }
+
+    void Jump()
+    {
+        Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
+
+        rb.velocity = movement;
+    }
+
+    //Check for collision with ground
+    public bool IsGrounded ()
+    {
+        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayers);
+
+        if (groundCheck != null)//Return true if player on the ground
+        {
+            return true;
+        }
+
+        return false;
     }
 }
